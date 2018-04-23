7b4133e7388965298ef78b2ac94be71deb32d957
Leonid Beder <leonid.beder@gmail.com>
2018-04-23 20:17:50 +0300

    Refactor Auction.sol

--- contracts/dos/Auction.sol
+++ contracts/dos/Auction.sol
@@ -5,12 +5,20 @@ contract Auction {
     address public currentLeader;
     uint256 public highestBid;
 
+    function Auction() public {
+        currentLeader = msg.sender;
+        highestBid = 0;
+    }
+
     function bid() public payable {
         require(msg.value > highestBid);
 
-        currentLeader.transfer(highestBid);
+        address prevCurrentLeader = currentLeader;
+        uint256 prevHighestBid = highestBid;
 
         currentLeader = msg.sender;
         highestBid = msg.value;
+
+        prevCurrentLeader.transfer(prevHighestBid);
     }
 }
